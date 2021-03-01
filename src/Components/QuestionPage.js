b6af2b72a55e55b06db82f83b4327381eb58c131
NathanielBannis <bannis52@gmail.com>
2021-03-01 16:51:24 -0500

    Deconstructed the classes to make the application more readable

--- src/Components/QuestionPage.js
+++ src/Components/QuestionPage.js
@@ -1,12 +1,14 @@
 //takes object and parses inputs: isdone, number of buttons and the text on the buttons
+//questionpage takes in an array of names, creates button objects from it.
 
 import React from 'react'
 import PropTypes from 'prop-types'
+import RoutingButtons from "./RoutingButtons";
 
-const QuestionPage = ({name}) => {
-    return <div>
+const QuestionPage = (props) => {
+    return<div>
 
-        <h1>{name}</h1>
+        <RoutingButtons button = {props.name}> </RoutingButtons>
 
     </div>
 }
@@ -15,6 +17,7 @@ QuestionPage.defaultProps = {
     name: 'hello',
 }
 
-QuestionPage.prototype = {name: PropTypes.string}
-
+QuestionPage.prototype = {name: PropTypes.Object}
+//buttonInfoObject: {name: string, [Object.typeOf(SpecialButton): {name: string, image: string}]
+//database button: {name: string, image: string, ArrayOfButtons : [name: String...]
 export default QuestionPage
--- src/Components/RoutingButtons.js
+++ src/Components/RoutingButtons.js
@@ -1,2 +1,10 @@
 //clicking a button will take you to the next page by adding to the person object and calling a function that will
 // rerender based upon changes to the person object (idea1)
+//function to send new props, function to fetch info from database
+
+
+const RoutingButton = (props) => {
+    return <Button> {props.name} </Button>
+}
+
+export default RoutingButton
