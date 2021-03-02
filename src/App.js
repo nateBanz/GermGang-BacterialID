715068711398483535a962368d42659ecdd9ad78
NathanielBannis <bannis52@gmail.com>
2021-03-02 08:25:51 -0500

    added more firebase attachments. Tested if the database was working with the app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import QuestionPage from "./Components/QuestionPage";
 import {useState} from "react";
+import firebase from './firebase';
 
 function App() {
   //const [specificGerm, updateGerm] = useState();
+
+  //test the firebase attachment
+  const firebaser = firebase.apps[0]
+  console.log(firebaser)
+
+
   return (
+      <div>
+
    <QuestionPage></QuestionPage>
+
+        <code>
+          <pre>{JSON.stringify(firebaser.options, null, 2)}</pre>
+        </code>
+
+</div>
   );
 }
 
--- src/Components/QuestionPage.js
+++ src/Components/QuestionPage.js
@@ -2,23 +2,29 @@
 //questionpage takes in an array of names, creates button objects from it.
 
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, {string} from 'prop-types'
 import RoutingButtons from "./RoutingButtons";
 import {useState} from 'react';
 //need some styling here to create the look we are going for
 
+let Person = {
+    name: string
+}
 //takes a parameter, an object defined below with the name of the germ, a picture and the list of germ names.
 const QuestionPage = (props) => {
     const [specificGerm, updateGerm] = useState(props);
 
-    console.log(specificGerm)
-    //the array of names stored here!
+    //now need an empty array to keep track of and change depending on the flowchart of the individual
+    const [flowOrder, updateFlow] = useState([]);
 
+    //need something to keep track of user. Passed from auth page to here in a specific set of props.
+
+
+    //the array of names stored here!
     const arrays = specificGerm.buttonList
-    console.log(arrays)
+
 
     //for every name in the array, create a div containing a button that passes in the name of the germ.
-
     return (arrays.map((list, index)=> <div className= "pages" key = {index}>
 
         <RoutingButtons button = {list} updateGerm = {updateGerm}> </RoutingButtons>
