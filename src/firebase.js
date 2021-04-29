094eab862a98fd1ba75286b1650c4e453593c399
sedinahus <shusanovic@ggc.edu>
2021-04-29 13:22:43 -0400

    secured api key

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,16 @@
 //for testing! Connects the app to firebase
-import "firebase/auth"
-import firebase from "firebase"
+import "firebase/auth";
+import firebase from "firebase";
+const APIKEY = require("./secert");
 
 firebase.initializeApp({
-
-    apiKey: "AIzaSyBcTLiaUw9N-4kle7OGzECw-UoJ6MVh2NY",
-    authDomain: "germgangpractice.firebaseapp.com",
-    projectId: "germgangpractice",
-    storageBucket: "germgangpractice.appspot.com",
-    messagingSenderId: "1060499409594",
-    appId: "1:1060499409594:web:dbc946b85a2c5231e01083"
-
+  apiKey: APIKEY.GOOGLE_API_KEY,
+  authDomain: "germgangpractice.firebaseapp.com",
+  projectId: "germgangpractice",
+  storageBucket: "germgangpractice.appspot.com",
+  messagingSenderId: "1060499409594",
+  appId: "1:1060499409594:web:dbc946b85a2c5231e01083",
 });
 
-
-export const auth = firebase.auth()
-export default firebase
+export const auth = firebase.auth();
+export default firebase;
